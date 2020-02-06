96cfac9a2256bcb7d5aced9e2ffb0ea72074b24a
Sean Van Osselaer <sean.vanosselaer@myob.com>
2020-02-06 16:22:24 +1100

    Adding 5 minute timeout to arachni example script

--- examples/arachni/run.sh
+++ examples/arachni/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-sudo OPENSSL_CONF=/etc/ssl/ /usr/lib/arachni-2.0dev-1.0dev/bin/arachni "http://security-testing-victim.local:3000"  --report-save-path ./report.afr --plugin=metrics
+sudo OPENSSL_CONF=/etc/ssl/ /usr/lib/arachni-2.0dev-1.0dev/bin/arachni "http://security-testing-victim.local:3000" --timeout=00:05:00 --report-save-path ./report.afr --plugin=metrics
 
 sudo /usr/lib/arachni-2.0dev-1.0dev/bin/arachni_reporter --reporter=html:outfile=report.html.zip ./report.afr
+
+unzip report.html.zip -d report
