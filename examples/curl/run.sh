f918ebff2a05c33ddf802535655bb110f1db1297
SVO <svo@qual.is>
2025-03-09 08:20:00 +1100

    Using specific private IP for curl scraping example

--- examples/curl/run.sh
+++ examples/curl/run.sh
@@ -3,5 +3,5 @@
 true > report-scraping-target.log
 
 for i in {1..500}; do
-  curl -I --silent --head -o /dev/null -w "%{http_code} - product ${i}\n" "http://172.17.0.1:3002/product/view?id=${i}" 2>&1 | tee -a report-scraping-target.log
+  curl -I --silent --head -o /dev/null -w "%{http_code} - product ${i}\n" "http://10.6.6.11/product/view?id=${i}" 2>&1 | tee -a report-scraping-target.log
 done
