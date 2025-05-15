33e1fc6a415ca413b8ffb8a4cae96f14390633df
SVO <svo@qual.is>
2025-05-15 13:08:16 +1000

    Cleanup of nmap examples

--- examples/nmap/run-ip-enumeration.sh
+++ examples/nmap/run-ip-enumeration.sh
@@ -2,4 +2,4 @@
 
 true > report-ip-enumeration.log
 
-nmap -sP 10.6.6.0/24 | tee report-ip-enumeration.log
+nmap -sn 10.6.6.0/24 | tee report-ip-enumeration.log
--- examples/nmap/run-udp.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-true > report-udp.log
-
-sudo nmap -Pn -sU -F 10.6.6.7 | tee report-udp.log
